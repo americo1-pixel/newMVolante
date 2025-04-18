f1aea667d9fb2cda93f42b782fb767a25962903f
chaskydev2 <chaskydev2@gmail.com>
2025-04-18 13:08:37 -0400

    clean master

--- lib/widget/firebase_pagination/src/functions/separator_builder.dart
+++
@@ -1,7 +0,0 @@
-// Flutter Packages
-import 'package:flutter/widgets.dart';
-
-/// Returns a [Widget] to be render as separator in a [ListView].
-Widget separatorBuilder(BuildContext context, int index) {
-  return const SizedBox.shrink();
-}
